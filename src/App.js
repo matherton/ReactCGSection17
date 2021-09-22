7daf8fbd5652ef42a88c6caf0a066536104f3286
Mark Atherton <mark@insightinternet.co.uk>
2021-09-22 13:26:54 +0100

    configured Prettier to run on file save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Layout/Header';
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
