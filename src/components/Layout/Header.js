83eaa1ea408e076031298e8a294268ebf10af92c
Mark Atherton <mark@insightinternet.co.uk>
2021-09-22 13:11:46 +0100

    end of lesson 132

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+import mealsBanner from '../../assets/meals.jpeg'
+
+const Header = props => {
+  return (
+      <>
+        <header className={styles.header}>
+          <h1>React meals!</h1>
+          <button>Cart</button>
+        </header>
+        <div className={styles['main-image']}>
+          <img src={mealsBanner} alt="" />
+        </div>
+      </>
+    )
+}
+
+export default Header;
