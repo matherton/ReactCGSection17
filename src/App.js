08e2e00af815eff1ec159a91c3856bccf7ba16f0
Mark Atherton <mark@insightinternet.co.uk>
2021-09-29 11:06:05 +0100

    end of lesson 140: added Cart component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+//import { useState } from "react/cjs/react.development";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState();
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
