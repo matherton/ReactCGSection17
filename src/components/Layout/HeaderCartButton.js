025651af33511d544e5ba769440edecc8c0e55b6
Mark Atherton <mark@insightinternet.co.uk>
2021-09-29 12:25:25 +0100

    end of lesson 142: added context to display {numberOfCardItems}

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
 
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCart.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCardItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCardItems}</span>
     </button>
   );
 };
