84f6c4811dda3215896ce684680b84c1b00fa520
Mark Atherton <mark@insightinternet.co.uk>
2021-10-04 16:34:28 +0100

    end of lesson 144: add to cart functionality using refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,23 @@
-import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
 
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
   const price = `£${props.price.toFixed(2)}`;
+
+  const CartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    CartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +26,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
