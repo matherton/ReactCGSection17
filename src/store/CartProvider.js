c9d1bbf720feb6f913f2225944b9ec591e655ffd
Mark Atherton <mark@insightinternet.co.uk>
2021-10-14 12:06:30 +0100

    added logic for if item exists in cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -36,6 +36,17 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+    } else {
+    }
+  }
   return defaultCartState;
 };
 
