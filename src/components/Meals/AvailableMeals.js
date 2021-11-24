56fee26dd94acf06675531bffa49fee973df514c
Mark Atherton <mark@insightinternet.co.uk>
2021-11-24 12:22:18 +0000

    end of lesson 215: missed out () in json() to execute the function on response as oppose to calling a json property of response without ()

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,13 +6,14 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
 
-      const responseData = await response.json;
+      const responseData = await response.json();
 
       const loadedMeals = [];
 
