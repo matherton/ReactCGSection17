636afa8b6981b3cb85dec0d0773b3ec214eafe53
Mark Atherton <mark@insightinternet.co.uk>
2021-11-29 11:11:38 +0000

    updated with Max's lesson start files

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -4,31 +4,31 @@ const Checkout = (props) => {
   const confirmHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your name</label>
+        <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
       </div>
-
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" />
       </div>
-
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" />
+      </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
-
-      <div className={classes.control}>
-        <label htmlFor="postcode">Post Code</label>
-        <input type="text" id="postcode" />
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
       </div>
-      <button type="submit">Confirm</button>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
     </form>
   );
 };
