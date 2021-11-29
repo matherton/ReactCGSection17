d6fe4758349c16332f2f7360e5280a9fa458750d
Mark Atherton <mark@insightinternet.co.uk>
2021-11-29 14:02:57 +0000

    finished lesson 221: now passing order data to firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -3,67 +3,68 @@ import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
 const isEmpty = (value) => value.trim() === "";
-const isFiveChars = (value) => value.trim().length === 6;
+const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
   const [formInputsValidity, setFormInputsValidity] = useState({
     name: true,
     street: true,
-    postal: true,
     city: true,
+    postalCode: true,
   });
+
   const nameInputRef = useRef();
   const streetInputRef = useRef();
-  const postalInputRef = useRef();
+  const postalCodeInputRef = useRef();
   const cityInputRef = useRef();
 
   const confirmHandler = (event) => {
     event.preventDefault();
+
     const enteredName = nameInputRef.current.value;
     const enteredStreet = streetInputRef.current.value;
-    const enteredPostal = postalInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
 
     const enteredNameIsValid = !isEmpty(enteredName);
     const enteredStreetIsValid = !isEmpty(enteredStreet);
     const enteredCityIsValid = !isEmpty(enteredCity);
-    const enteredPostalIsValid = isFiveChars(enteredPostal);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
 
     setFormInputsValidity({
       name: enteredNameIsValid,
       street: enteredStreetIsValid,
       city: enteredCityIsValid,
-      postal: enteredPostalIsValid,
+      postalCode: enteredPostalCodeIsValid,
     });
 
-    const isFormValid =
+    const formIsValid =
       enteredNameIsValid &&
       enteredStreetIsValid &&
       enteredCityIsValid &&
-      enteredPostalIsValid;
+      enteredPostalCodeIsValid;
 
-    if (!isFormValid) {
-      /* throw new Error(
-        "Delivery details you have provide are invalid! Please re-ebnter delivery details"
-      ); */
-      console.log(
-        "Delivery details you have provide are invalid! Please re-ebnter delivery details"
-      );
+    if (!formIsValid) {
+      return;
     }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode,
+    });
   };
 
   const nameControlClasses = `${classes.control} ${
     formInputsValidity.name ? "" : classes.invalid
   }`;
-
   const streetControlClasses = `${classes.control} ${
     formInputsValidity.street ? "" : classes.invalid
   }`;
-
-  const postalControlClasses = `${classes.control} ${
-    formInputsValidity.name ? "" : classes.invalid
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
   }`;
-
   const cityControlClasses = `${classes.control} ${
     formInputsValidity.city ? "" : classes.invalid
   }`;
@@ -80,11 +81,11 @@ const Checkout = (props) => {
         <input type="text" id="street" ref={streetInputRef} />
         {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
-      <div className={postalControlClasses}>
+      <div className={postalCodeControlClasses}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" ref={postalInputRef} />
-        {!formInputsValidity.postal && (
-          <p>Please enter a valid postal code! (6 characters)</p>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>Please enter a valid postal code (5 characters long)!</p>
         )}
       </div>
       <div className={cityControlClasses}>
